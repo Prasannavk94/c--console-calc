017dee72791bf4b0a44696180c84dd3579c9431f
Prasanna V <prasannav487@gmail.com>
2025-02-07 03:22:13 +0530

    Calculator created with basic functions

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleCalculator;
+
+internal static class Calculator
+{
+    public static void Main(string[] args)
+    {
+        var helper = new IoHelper();
+        helper.WriteLine("Please enter a number: ");
+        double num1 = double.Parse(helper.ReadLine());
+        helper.WriteLine("Please enter a operation: ");
+        string operation = helper.ReadLine();
+        helper.WriteLine("Please enter a number: ");
+        double num2 = double.Parse(helper.ReadLine());
+        try
+        {
+            double result = operation switch
+            {
+                "+" => num1 + num2,
+                "-" => num1 - num2,
+                "*" => num1 * num2,
+                "/" => num1 / num2,
+                "%" => num1 % num2,
+                _ => throw new Exception("Invalid operation")
+            };
+            Console.WriteLine("The result is: " + result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ IOHelper.cs
@@ -0,0 +1,7 @@
+namespace ConsoleCalculator;
+
+public class IoHelper
+{
+    public void WriteLine(string line) => Console.WriteLine(line);
+    public string ReadLine() => Console.ReadLine();
+}
